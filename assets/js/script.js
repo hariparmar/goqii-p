741180cc7aa612ca3b10032c13b302ee122ed2c0
Nikhil Parmar <nikhilparmar92@gmail.com>
2021-09-13 15:11:00 +0530

    home page mobile responsive just last section is remaining
    home page mobile responsive, just last section is remaining

--- assets/js/script.js
+++ assets/js/script.js
@@ -155,6 +155,11 @@ const blogTestimonialsSlider = new Swiper(".blog-testimonials-slider", {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
     },
+    breakpoints: {
+        767: {
+            spaceBetween: 5
+        }
+    },
     // If we need pagination
     pagination: {
         el: ".swiper-pagination",
@@ -229,9 +234,9 @@ const blogBanenerSlider = new Swiper(".blog-banner__inner__slider", {
 });
 
 //  date picker - blog page
-$(function () {
-  $("#datepicker").datepicker({ 
-        autoclose: true, 
+$(function() {
+    $("#datepicker").datepicker({
+        autoclose: true,
         todayHighlight: true
-  }).datepicker('update', new Date());
+    }).datepicker('update', new Date());
 });
