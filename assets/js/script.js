5f53617861fbea5d2eb886d411db29960a483afb
Nikhil Parmar <nikhilparmar92@gmail.com>
2021-10-27 17:58:56 +0530

    Phase 3- corporate wellness, quiz popup, responsive
    Phase 3- corporate wellness, quiz popup, responsive, deleted startup development page as it's not required

--- assets/js/script.js
+++ assets/js/script.js
@@ -179,11 +179,35 @@ const blogTestimonialsSlider = new Swiper(".blog-testimonials-slider", {
         clickable: true
     },
 });
+
+// Magnific popup
 $(document).ready(function() {
     $('.popup-youtube').magnificPopup({
         type: 'iframe'
     });
+
+    $('.popup-with-form').magnificPopup({
+        type: 'inline',
+        preloader: false,
+        focus: '#name',
+
+        // When elemened is focused, some mobile browsers in some cases zoom in
+        // It looks not nice, so we disable it:
+        callbacks: {
+            beforeOpen: function() {
+                if ($(window).width() < 700) {
+                    this.st.focus = false;
+                } else {
+                    this.st.focus = '#name';
+                }
+            }
+        }
+    });
 });
+
+
+
+
 
 //  About Team slider
 const aboutLeaderThumbSlider = new Swiper(".about-team-slider__inner__thumb--leadership", {
